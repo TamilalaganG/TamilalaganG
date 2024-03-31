c68749176e3d1dbc0d8187950c9d1a0985657961
TamilalaganG <153286765+TamilalaganG@users.noreply.github.com>
2024-03-31 18:49:14 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,1 +1,28 @@
-git add123
+package myJavaProgram;
+
+public class ArraySorting {
+	public static void main(String[] args) {
+	//String s1[] = {"Lion","Tiger","Camel","Cow","Fox","Ox"};
+	
+	//String s2 = "eqknaocnaocn_#$%^&1234";
+	
+	//How to get India option in an country drop down - Selenium Question
+	
+	String s2 = "_#$%^&1234eqkn$%aocn^&*aocn";
+	
+    // Iterate over each character in the string
+    for (int i = 0; i < s2.length(); i++) {
+        char ch = s2.charAt(i);
+       // System.out.println(ch);
+        // Check if the character is an alphabet
+        if (Character.isLetter(ch)) {
+            // Print the alphabet
+            System.out.print(ch); 
+            
+}
+         
+}
+  
+}
+	
+}
